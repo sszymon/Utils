70d9720bb608eee777695a66287462081584f8a6
Szymon Suszko <s.suszko@bankier.pl>
2019-06-25 08:58:52 +0200

    clear

--- git.aliases.sh
+++ git.aliases.sh
@@ -3,6 +3,5 @@ git config --global alias.s status &&
 git config --global alias.sh stash &&
 git config --global alias.shp 'stash pop' && 
 git config --global alias.f fetch &&  
-git config --global alias.a 'add . ' && 
-git config --global alias.xx xx &&
+git config --global alias.a 'add . ' &&
 git config --global alias.c 'commit -m '
