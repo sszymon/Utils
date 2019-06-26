937cbaf1332b703132aca04a9c69b918456bc0f3
Szymon Suszko <szymon.suszko@gmail.com>
2019-06-26 04:56:53 +0200

    Update git.aliases.sh

--- git.aliases.sh
+++ git.aliases.sh
@@ -2,6 +2,7 @@ git config --global alias.co checkout &&
 git config --global alias.s status && 
 git config --global alias.sh stash &&
 git config --global alias.shp 'stash pop' && 
+git config --global alias.b branch &&  
 git config --global alias.f fetch &&  
 git config --global alias.a 'add . ' &&
 git config --global alias.c 'commit -m '
