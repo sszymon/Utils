575ddab43f6203dd9e057068b467ab589cef1778
Szymon Suszko <szymon.suszko@gmail.com>
2022-07-04 12:04:29 +0200

    make it executable

--- git.aliases.sh
+++ git.aliases.sh
@@ -1,8 +1,12 @@
-git config --global alias.co checkout && 
-git config --global alias.s status && 
-git config --global alias.sh stash &&
-git config --global alias.shp 'stash pop' && 
-git config --global alias.b branch &&  
-git config --global alias.f fetch &&  
-git config --global alias.a 'add . ' &&
-git config --global alias.c 'commit -m '
+#!/bin/bash
+
+$ git config --global alias.co checkout
+$ git config --global alias.s status
+$ git config --global alias.sh stash
+$ git config --global alias.shp 'stash pop'
+$ git config --global alias.b branch
+$ git config --global alias.f fetch
+$ git config --global alias.a 'add . '
+$ git config --global alias.c 'commit -m '
+
+echo 'Setting Git aliases done'
